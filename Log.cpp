a1b45f71be98a2cdc5cc34b1994c87781d4455e6
fireworkada <2830728655@qq.com>
2024-03-25 15:58:57 +0800

    add a test Log.cpp file

---
+++ Log.cpp
@@ -0,0 +1,13 @@
+//
+//  Log.cpp
+//  firework
+//
+//  Created by Ada on 2024/3/23.
+//
+
+#include "Log.hpp"
+#include <iostream>
+void Log(const char* message)
+{
+    std::cout<<message<<std::endl;
+}
